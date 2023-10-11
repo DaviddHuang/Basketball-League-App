834d6ad798efc0b63e09c3de991f034a6cd1ceb5
David Huang <dhuang13@students.cs.ubc.ca>
2023-10-10 22:11:12 -0700

    Implemented ui and created new class BasketballLeagueApp which main runs and added appropriate documentation and code references

---
+++ src/main/ui/BasketballLeagueApp.java
@@ -0,0 +1,342 @@
+package ui;
+
+import model.League;
+import model.Player;
+import model.Team;
+
+import java.util.*;
+
+// Basketball league application
+public class BasketballLeagueApp {
+    private Scanner input;
+    private Team funTeam;
+    private League funLeague;
+    private Player myPlayer;
+
+    public BasketballLeagueApp() {
+        startApp();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: read and use the user inputs
+    // Code referenced from https://github.students.cs.ubc.ca/CPSC210/TellerApp.git from the tellerApp class in the ui
+    // package, method: runTeller()
+    private void startApp() {
+        boolean notOver = true;
+        String userInput = null;
+
+        initializeLeague();
+
+        System.out.println("\nWelcome to the Basketball League Tracker!");
+        System.out.println("-----------------------------------------");
+        startMenu();
+
+        while (notOver) {
+            userInput = input.next();
+            userInput = userInput.toLowerCase();
+
+            if (userInput.equals("quit")) {
+                notOver = false;
+                break;
+            } else {
+                readInputs(userInput);
+            }
+        }
+
+        System.out.println("It's sad to see you go...");
+        System.exit(0);
+    }
+
+    // EFFECTS: displays the start menu to user
+    // Code referenced from https://github.students.cs.ubc.ca/CPSC210/TellerApp.git from the tellerApp class in the ui
+    // package, method: displayMenu()
+    private void startMenu() {
+        System.out.println("Type 'Start' to create your own league: ");
+        System.out.println("Type 'Quit' to exit now: ");
+    }
+
+    // MODIFIES: this
+    // EFFECTS: reads the input from the user and does the next command
+    // Code referenced from https://github.students.cs.ubc.ca/CPSC210/TellerApp.git from the tellerApp class in the ui
+    // package, method: processCommand()
+    private void readInputs(String userInput) {
+        if (userInput.equals("start")) {
+            createLeague();
+        } else {
+            System.out.println("Invalid input, please try again.\n");
+            startMenu();
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes a scanner for input
+    // Code referenced from https://github.students.cs.ubc.ca/CPSC210/TellerApp.git from the tellerApp class in the ui
+    // package, method: init()
+    private void initializeLeague() {
+        input = new Scanner(System.in);
+        input.useDelimiter("\n");
+    }
+
+
+    // MODIFIES: this
+    // EFFECTS: takes a name from the user and creates a league with a name then continues with creating a team
+    private void createLeague() {
+        System.out.println("Please enter the name of your league: ");
+        String name = input.next();
+
+        if (!name.isEmpty()) {
+            funLeague = new League(name);
+            System.out.println("League added: " + name);
+            createTeam();
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: take a name from the user and creates a team with that name then adds it to the league then displays
+    //          team menu
+    private void createTeam() {
+        System.out.println("Please enter the name of your team: ");
+        String name = input.next();
+
+        if (!name.isEmpty()) {
+            funTeam = new Team(name);
+            System.out.println("Team added: " + name);
+            funLeague.addTeam(funTeam);
+            teamMenu();
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: gives prompts for user inputs for name, jersey number, height, and weight, and creates a player using
+    //          the given inputs. Adds player to the current team and displays player menu
+    private void createPlayer() {
+        System.out.println("Please enter the name of a player you want to add to the roster: ");
+        String name = input.next();
+        System.out.println("Please enter the players jersey number: ");
+        int jersey = input.nextInt();
+        checkSameJersey(jersey);
+        System.out.println("Please enter the players height(cm): ");
+        int height = input.nextInt();
+        System.out.println("Please enter the players weight(lbs): ");
+        double weight = input.nextDouble();
+        myPlayer = new Player(name, jersey, height, weight);
+        funTeam.addPlayer(myPlayer);
+        System.out.println("Player added: " + myPlayer.getName());
+        playerMenu();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: asks user for input for player they want to remove, takes input and removes player from the team then
+    //          prompts player menu
+    private void removePlayer() {
+        System.out.println("Please enter the name of a player on your roster you wish to remove: ");
+        String name = input.next();
+        funTeam.removePlayer(name);
+        System.out.println("Played removed: " + name);
+        playerMenu();
+    }
+
+    // EFFECTS: displays options that a user has when interacting with a player
+    // Code referenced from https://github.students.cs.ubc.ca/CPSC210/TellerApp.git from the tellerApp class in the ui
+    // package, method: runTeller()
+    private void playerMenu() {
+        boolean menuNotOver = true;
+        playerMenuPrints();
+
+        while (menuNotOver) {
+            String newPlayer = input.next();
+            newPlayer = newPlayer.toLowerCase();
+            if (newPlayer.equals("new")) {
+                createPlayer();
+            } else if (newPlayer.equals("back")) {
+                teamMenu();
+            } else if (newPlayer.equals("view")) {
+                displayRosterInfo();
+                playerMenu();
+            } else if (newPlayer.equals("select player")) {
+                isRosterEmptySelectPlayer();
+            } else if (newPlayer.equals("remove player")) {
+                isRosterEmptyRemovePlayer();
+            } else {
+                System.out.println("Invalid Selection...");
+                playerMenu();
+            }
+        }
+    }
+
+    // EFFECTS: displays options that a user has when interacting with a team
+    // Code referenced from https://github.students.cs.ubc.ca/CPSC210/TellerApp.git from the tellerApp class in the ui
+    // package method: runTeller()
+    private void teamMenu() {
+        boolean teamMenuNotOver = true;
+        teamMenuPrints();
+
+        while (teamMenuNotOver) {
+            String newTeam = input.next();
+            newTeam = newTeam.toLowerCase();
+            if (newTeam.equals("new")) {
+                createTeam();
+            } else if (newTeam.equals("menu")) {
+                startApp();
+            } else if (newTeam.equals("continue")) {
+                System.out.println("Current Team Selected: " + funTeam.getTeamName());
+                playerMenu();
+            } else if (newTeam.equals("view")) {
+                displayTeamInfo();
+                teamMenu();
+            } else if (newTeam.equals("select team")) {
+                displayTeamInfo();
+                selectTeamUseAsInput();
+            } else {
+                System.out.println("Invalid Selection...");
+                teamMenu();
+            }
+        }
+    }
+
+    // EFFECTS: display the players on the roster with their name, jersey number, average points, rebounds, and
+    //          assists
+    private void displayRosterInfo() {
+        if (funTeam.getRoster().isEmpty()) {
+            System.out.println("The roster is empty.");
+        }
+        System.out.println("Current Roster: ");
+        for (Player p : funTeam.getRoster()) {
+            System.out.println("Name: " + p.getName() + " ||" + " Jersey Number: " + p.getJerseyNumber()
+                    + " ||" + " Height: " + p.getHeight() + "cm" + " ||" + " Weight: " + p.getWeight() + "lbs" + " ||"
+                    + " PPG: " + p.averagePoints() + " ||" + " RPG: " + p.averageRebounds() + " ||" + " APG: "
+                    + p.averageAssists() + " ||" + " GP: " + p.getGamesPlayed());
+        }
+    }
+
+    // EFFECTS: displays the teams name, wins, and losses
+    private void displayTeamInfo() {
+        System.out.println("Current Teams: ");
+        for (Team t : funLeague.getTeams()) {
+            System.out.println("Team: " + t.getTeamName() + " ||" + " Wins: " + t.getWins() + " ||" + " Losses: "
+                     + t.getLosses());
+        }
+    }
+
+    // EFFECTS: prints team menu options
+    // Code referenced from https://github.students.cs.ubc.ca/CPSC210/TellerApp.git from the tellerApp class in the ui
+    // package, method: displayMenu()
+    private void teamMenuPrints() {
+        System.out.println("\nType 'New' to add a new team: ");
+        System.out.println("Type 'Continue' to add players to the current team: ");
+        System.out.println("Type 'View' to view a list of teams: ");
+        System.out.println("Type 'Select Team' to add players to a different team: ");
+        System.out.println("Type 'Menu' to go back to the main menu: ");
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes the current team to the team with the given name, if there are no current teams give prompt
+    //          and ask for user input again
+    private void selectTeam(String name) {
+        for (Team t: funLeague.getTeams()) {
+            if (t.getTeamName().equalsIgnoreCase(name)) {
+                System.out.println("Current Team Selected: " + t.getTeamName());
+                funTeam = t;
+                playerMenu();
+            }
+        }
+        System.out.println("Team not found...");
+        selectTeamUseAsInput();
+    }
+
+    // EFFECTS: processes the user's input when asked to select a team
+    private void selectTeamUseAsInput() {
+        System.out.println("Type the name of the team you wish to select: ");
+        String teamInput = input.next();
+        teamInput = teamInput.toLowerCase();
+        selectTeam(teamInput);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: changes the current player to the player with the given name to add stats for them, if there is no
+    //          players on a team give user a prompt and ask for user input again
+    private void selectPlayer(String name) {
+        for (Player p : funTeam.getRoster()) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                System.out.println("Current Player Selected: " + p.getName());
+                myPlayer = p;
+                addStatsForPlayer();
+            }
+        }
+        System.out.println("Player not found...");
+        selectPlayerInput();
+    }
+
+    // EFFECTS: takes user input on selected player
+    private void selectPlayerInput() {
+        System.out.println("Type the name of the player you wish to select: ");
+        String playerInput = input.next();
+        playerInput = playerInput.toLowerCase();
+        selectPlayer(playerInput);
+    }
+
+    // EFFECTS: displays print statements for the player menu
+    // Code referenced from https://github.students.cs.ubc.ca/CPSC210/TellerApp.git from the tellerApp class in the ui
+    // package, method: displayMenu()
+    private void playerMenuPrints() {
+        System.out.println("\nType 'New' to add new player: ");
+        System.out.println("Type 'View' to view the current roster: ");
+        System.out.println("Type 'Select Player' for the player you wish to add stats for: ");
+        System.out.println("Type 'Remove Player' for the player you wish to remove: ");
+        System.out.println("Type 'Back' to go back: ");
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: gives prompt for user input for latest amount of points, rebounds, assists, and games played for the
+    //          current player, takes user input and updates the current stats of the player
+    private void addStatsForPlayer() {
+        System.out.println("Please enter the latest amount of points for this player: ");
+        int points = input.nextInt();
+        System.out.println("Please enter the latest amount of rebounds for this player: ");
+        int rebounds = input.nextInt();
+        System.out.println("Please enter the latest amount of assists for this player: ");
+        int assists = input.nextInt();
+        System.out.println("Please enter the latest amount of games played for this player: ");
+        int gamesPlayed = input.nextInt();
+        myPlayer.playGame(points, rebounds, assists, gamesPlayed);
+        System.out.println("Player stats have been updated for player: " + myPlayer.getName());
+        playerMenu();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: checks if the roster is currently empty, display the current roster and prompts user to
+    //          select a player
+    private void isRosterEmptySelectPlayer() {
+        if (funTeam.getRoster().isEmpty()) {
+            System.out.println("The roster is currently empty...");
+            playerMenu();
+        }
+        displayRosterInfo();
+        selectPlayerInput();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: checks if the roster is currently empty, display the current roster and prompts user to
+    //          remove a player
+    private void isRosterEmptyRemovePlayer() {
+        if (funTeam.getRoster().isEmpty()) {
+            System.out.println("The roster is currently empty...");
+            playerMenu();
+        }
+        displayRosterInfo();
+        removePlayer();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: checks if players on the roster have same jersey as the given jersey, create a new player if the
+    //          jersey's match
+    private void checkSameJersey(int jersey) {
+        for (Player p : funTeam.getRoster()) {
+            if (p.getJerseyNumber() == jersey) {
+                System.out.println("Player added unsuccessfully: That jersey is already taken. Please try again!");
+                createPlayer();
+            }
+        }
+    }
+}
